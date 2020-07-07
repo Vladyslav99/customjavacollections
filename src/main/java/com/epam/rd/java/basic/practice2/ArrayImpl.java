bfc388565c665d5dfa519903cbea161c6a6bc2b1
HBLdNnRZ <vladyslavkondratenko24@gmail.com>
2020-07-07 17:23:58 +0300

    ArrayImpl without autoExtension

--- src/main/java/com/epam/rd/java/basic/practice2/ArrayImpl.java
+++ src/main/java/com/epam/rd/java/basic/practice2/ArrayImpl.java
@@ -1,69 +1,158 @@
 package com.epam.rd.java.basic.practice2;
 
+import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.List;
+import java.util.NoSuchElementException;
 
 public class ArrayImpl implements Array {
 
-	@Override
-    public void clear() {
-        
+    private static final int DEFAULT_CAPACITY = 10;
+
+    private Object[] objectData;
+
+    private int size;
+
+    public ArrayImpl() {
+        objectData = new Object[DEFAULT_CAPACITY];
     }
 
-	@Override
+    public ArrayImpl(int initialCapacity) {
+        if (initialCapacity < 0) {
+            throw new IllegalArgumentException("Illegal Capacity: " + initialCapacity);
+        }
+        objectData = new Object[initialCapacity];
+    }
+
+    @Override
+    public void clear() {
+        for (int i = 0; i < size; i++) {
+            objectData[i] = null;
+        }
+
+        size = 0;
+    }
+
+    @Override
     public int size() {
-        return 0;
+        return size;
     }
-	
-	@Override
+
+    @Override
     public Iterator<Object> iterator() {
-	    return new IteratorImpl();
+        return new IteratorImpl();
     }
-	
-	private class IteratorImpl implements Iterator<Object> {
+
+    private class IteratorImpl implements Iterator<Object> {
+
+        int cursor;
 
         @Override
         public boolean hasNext() {
-            return false;
+            return cursor != size;
         }
 
         @Override
         public Object next() {
-            return null;
+            if (cursor == size) {
+                throw new NoSuchElementException();
+            }
+
+            return objectData[cursor++];
         }
 
     }
-	
-	@Override
+
+    @Override
     public void add(Object element) {
-        
+        if (size == objectData.length) {
+            throw new IndexOutOfBoundsException();
+        }
+        objectData[size++] = element;
     }
 
-	@Override
+    @Override
     public void set(int index, Object element) {
-        
+        if (index < 0 || index > objectData.length - 1) {
+            throw new IllegalArgumentException("Illegal Index: " + index);
+        }
+
+        if (index > size) {
+            throw new IndexOutOfBoundsException();
+        }
+
+        objectData[index] = element;
+
     }
 
-	@Override
+    @Override
     public Object get(int index) {
-        return null;
+
+        if (index < 0 || index > objectData.length - 1) {
+            throw new IllegalArgumentException("Illegal Index: " + index);
+        }
+
+        if (index > size) {
+            throw new IndexOutOfBoundsException();
+        }
+
+        return objectData[index];
     }
 
-	@Override
+    @Override
     public int indexOf(Object element) {
-        return 0;
+
+        for (int i = 0; i < size; i++) {
+            if (element.equals(objectData[i])) {
+                return i;
+            }
+        }
+
+        throw new NoSuchElementException();
     }
 
-	@Override
+    @Override
     public void remove(int index) {
-        
+        if (index < 0 || index > objectData.length - 1) {
+            throw new IllegalArgumentException("Illegal Index: " + index);
+        }
+
+        if (index > size) {
+            throw new IndexOutOfBoundsException();
+        }
+
+        objectData[index] = null;
+        System.arraycopy(objectData, index + 1, objectData, index, --size - index);
     }
 
     @Override
     public String toString() {
-        return null;
+
+        StringBuilder stringBuilder = new StringBuilder();
+
+        Iterator<Object> iterator = iterator();
+
+        while (iterator.hasNext()) {
+            stringBuilder.append(" " + iterator.next().toString());
+        }
+
+        return stringBuilder.toString().trim();
     }
 
     public static void main(String[] args) {
+
+        ArrayImpl array = new ArrayImpl();
+
+
+        for (int i = 0; i < 7; i++) {
+            array.add(i);
+        }
+
+        System.out.println(array.toString());
+
+        array.set(2, 100);
+
+        System.out.println(array.toString());
 
     }
 
