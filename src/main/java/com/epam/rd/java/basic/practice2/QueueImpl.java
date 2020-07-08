28888b1c9f806b3f2aacfce09728640ce79565ad
HBLdNnRZ <vladyslavkondratenko24@gmail.com>
2020-07-08 16:50:37 +0300

    QueueImpl and StackImpl completed

--- src/main/java/com/epam/rd/java/basic/practice2/QueueImpl.java
+++ src/main/java/com/epam/rd/java/basic/practice2/QueueImpl.java
@@ -1,18 +1,31 @@
 package com.epam.rd.java.basic.practice2;
 
 import java.util.Iterator;
+import java.util.NoSuchElementException;
 
 public class QueueImpl implements Queue {
+
+    private static final int INITIAL_CAPACITY = 10;
+
+    private int total;
+    private int first;
+    private int next;
+    private Object[] elementData;
+
+    public QueueImpl() {
+        elementData = new Object[INITIAL_CAPACITY];
+    }
 
 
     @Override
     public void clear() {
-
+        elementData = new Object[INITIAL_CAPACITY];
+        total = first = next = 0;
     }
 
     @Override
     public int size() {
-        return 0;
+        return total;
     }
 
     public Iterator<Object> iterator() {
@@ -21,39 +34,87 @@ public class QueueImpl implements Queue {
 
     private class IteratorImpl implements Iterator<Object> {
 
+        int cursor;
+
         @Override
         public boolean hasNext() {
-            return false;
+            return cursor != total;
         }
 
         @Override
         public Object next() {
-            return null;
+            if (!hasNext()) {
+                throw new NoSuchElementException();
+            }
+
+            return elementData[(first + cursor++) % elementData.length];
         }
 
     }
 
     @Override
     public void enqueue(Object element) {
+        if (elementData.length == total) {
+            resize(elementData.length * 2);
+        }
+        elementData[next++] = element;
+        if (next == elementData.length) {
+            next = 0;
+        }
+        total++;
+    }
 
+    private void resize(int capacity) {
+        Object[] tempElements = new Object[capacity];
+
+        for (int i = 0; i < total; i++) {
+            tempElements[i] = elementData[(first + i) % elementData.length];
+        }
+
+        elementData = tempElements;
+        first = 0;
+        next = total;
     }
 
     @Override
     public Object dequeue() {
-        return null;
+
+        if (total == 0) {
+            throw new NoSuchElementException();
+        }
+        Object element = elementData[first];
+        elementData[first] = null;
+
+        if (++first == elementData.length) {
+            first = 0;
+        }
+
+        if (--total > 0 && total == elementData.length / 4) {
+            resize(elementData.length / 2);
+        }
+
+        return element;
     }
 
     @Override
     public Object top() {
-        return null;
+        return elementData[first];
     }
 
     @Override
     public String toString() {
-        return null;
+
+        StringBuilder stringBuilder = new StringBuilder();
+
+        for (int i = 0; i < total; i++) {
+            stringBuilder.append(", " + elementData[(first + i) % elementData.length]);
+        }
+
+        return stringBuilder.toString().replaceFirst(",", "").trim();
     }
 
 
+    @SuppressWarnings("all")
     public static void main(String[] args) {
 
     }
--- src/main/java/com/epam/rd/java/basic/practice2/StackImpl.java
+++ src/main/java/com/epam/rd/java/basic/practice2/StackImpl.java
@@ -1,18 +1,27 @@
 package com.epam.rd.java.basic.practice2;
 
 import java.util.Iterator;
+import java.util.NoSuchElementException;
 
 public class StackImpl implements Stack {
 
+    private int top;
+    private Object[] elementData;
+    private static final int INITIAL_CAPACITY = 16;
+
+    public StackImpl() {
+        elementData = new Object[INITIAL_CAPACITY];
+    }
 
     @Override
     public void clear() {
-
+        elementData = new Object[INITIAL_CAPACITY];
+        top = 0;
     }
 
     @Override
     public int size() {
-        return 0;
+        return top;
     }
 
     public Iterator<Object> iterator() {
@@ -21,38 +30,72 @@ public class StackImpl implements Stack {
 
     private class IteratorImpl implements Iterator<Object> {
 
+        int cursor;
+
+        public IteratorImpl() {
+            cursor = top;
+        }
+
         @Override
         public boolean hasNext() {
-            return false;
+            return cursor != 0;
         }
 
         @Override
         public Object next() {
-            return null;
+            if (!hasNext()) {
+                throw new NoSuchElementException();
+            }
+            return elementData[--cursor];
         }
 
     }
 
     @Override
     public void push(Object element) {
+        if (top == elementData.length) {
+            expandSize();
+        }
+        elementData[top++] = element;
+    }
 
+    private void expandSize() {
+        int newSize = (int) (elementData.length * 1.5);
+        Object[] elements = elementData;
+        elementData = new Object[newSize];
+        System.arraycopy(elements, 0, elementData, 0, elements.length);
     }
 
     @Override
     public Object pop() {
-        return null;
+        if (top == 0) {
+            throw new NoSuchElementException();
+        }
+        Object object = elementData[--top];
+        elementData[top] = null;
+        return object;
     }
 
     @Override
     public Object top() {
-        return null;
+        if (top == 0) {
+            throw new NoSuchElementException();
+        }
+        return elementData[top - 1];
     }
 
     @Override
     public String toString() {
-        return null;
+        StringBuilder stringBuilder = new StringBuilder();
+
+        for (int i = 0; i < top; i++) {
+            stringBuilder.append(", " + elementData[i]);
+        }
+
+        return stringBuilder.toString().replaceFirst(",", "").trim();
     }
 
+    @SuppressWarnings("all")
     public static void main(String[] args) {
 
     }
